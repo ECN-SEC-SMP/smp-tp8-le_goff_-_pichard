718772508b771308741d3306dbf6deee20d74101
Olwen <nonopich@proton.me>
2026-03-15 10:07:25 +0100

    Modification de la fonction de déplacement du loup

--- Wolf.cpp
+++ Wolf.cpp
@@ -12,25 +12,8 @@ void Wolf::deplace(int maxX, int maxY)  // Création de la méthode deplacement 
     int typeX = rand() % maxX;          // Génération d'un déplacement aléatoire en x
     int typeY = rand() % maxY;          // Génération d'un déplacement aléatoire en y
 
-    this->x = this->x + typeX;          // Le loup se déplace en x + typeX (on ajoute pour que le déplacement soit vers la droite du plateau)
-    if (this->x > maxX)                 // On vérifie que le loup ne dépasse pas la limite maxX (à droite du plateau)
-    {
-        this->x = this->x - maxX;       // Si c'est le cas, on le fait revenir à l'autre bout du plateau moins maxX
-    }
-    else if (this->x < 0)               // On vérifie que le loup ne dépasse pas la limite 0 (à gauche du plateau)
-    {
-        this->x = this->x - maxX;       // Si c'est le cas, on le fait revenir à l'autre bout du plateau moins maxX
-    }
-
-    this->y = this->y - typeY;          // Le loup se déplace en y - typeY (on soustrait pour que le déplacement soit vers le haut du plateau)
-    if (this->y < 0)                    // On vérifie que le loup ne dépasse pas la limite 0 (en haut du plateau)   
-    {
-        this->y = this->y + maxY;       // Si c'est le cas, on le fait revenir à l'autre bout du plateau + maxY
-    }
-    else if (this->y > maxY)            // On vérifie que le loup ne dépasse pas la limite maxY (en bas du plateau)
-    {
-        this->y = this->y - maxY;       // Si c'est le cas, on le fait revenir à l'autre bout du plateau moins maxY
-    }
+    this->x = typeX;
+    this->y = typeY;
 }
 
 Wolf::Wolf(int maxX, int maxY) : Animal(maxX, maxY) // Création du constructeur pour le loup qui appelle le constructeur de la classe mère Animal
